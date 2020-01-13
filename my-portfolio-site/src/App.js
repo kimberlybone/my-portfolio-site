0f1f2ee5429f7e01740d3dcbe04942da06961df4
kimberlybone <k_bone@uncg.edu>
2020-01-13 15:02:09 -0500

    added to state

--- my-portfolio-site/src/App.js
+++ my-portfolio-site/src/App.js
@@ -5,10 +5,32 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-
+      title: "Kimberly Bone",
+      headerLinks: [
+        { title: "Welcome", path: "/" },
+        { title: "What I Do", path: "/what" },
+        { title: "Who I Am", path: "/who" },
+        { title: "Contact Me", path: "/contact" }
+      ],
+      welcome: {
+        title: "Welcome",
+        subTitle: "A little bit about me...",
+        subText: "Checkout my projects"
+      },
+      what: {
+        title: "What I Do",
+        subTitle: "A little bit about me...",
+        subText: "Checkout my projects"
+      },
+      who: {
+        title: "Who I am"
+      },
+      contact: {
+        title: "Let's Connect"
+      }
     }
   }
-  
+
   render() {
     return(
       <div className="App">
