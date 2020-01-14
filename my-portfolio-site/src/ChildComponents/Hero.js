f9c612fdf77a12d8946ecc27dcbf68137554d849
kimberlybone <k_bone@uncg.edu>
2020-01-13 22:06:41 -0500

    add s3 bucket

--- my-portfolio-site/src/ChildComponents/Hero.js
+++ my-portfolio-site/src/ChildComponents/Hero.js
@@ -8,7 +8,7 @@ function Hero( props ){
         <Row className="justify-content-center py-5">
           <Col md={ 5 } sm={ 5 }>
           { props.title && <h1 className="display-1 font-weight-bold"> { props.title } </h1> }
-          { props.subTitle && <h3 className="display-3 font-weight-light"> { props.subTitle } </h3> }
+          { props.subTitle && <h3 className="display-4 font-weight-light"> { props.subTitle } </h3> }
           { props.subText && <h3 className="lead font-weight-light"> { props.subText } </h3> }
           </Col>
         </Row>
