f2cc598ef0dbd03b12734437ed249118f00abf75
kimberlybone <k_bone@uncg.edu>
2020-01-14 14:30:07 -0500

    added custom css

--- my-portfolio-site/src/ChildComponents/CardInfo.js
+++ my-portfolio-site/src/ChildComponents/CardInfo.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring'
+import { Link } from 'react'
 
 function CardInfo( props ){
 
-
+  const style = useSpring({ opacity: 1, from: { opacity: 0 } })
     return(
-      <div>
-        CardInfo
-      </div>
+      <animated.div className="" style= { style }>
+        <p>{ props.title }</p>
+        <p>{ props.subTitle }</p>
+        <Link>{ props.link }</Link>
+      </animated.div >
     )
 
 }
