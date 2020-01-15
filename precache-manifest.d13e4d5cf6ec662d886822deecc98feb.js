362a882ee1c3bf2e8a2a291e5d5940e1feb20468
kimberlybone <k_bone@uncg.edu>
2020-01-15 11:09:45 -0500

    Updates

---
+++ precache-manifest.d13e4d5cf6ec662d886822deecc98feb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd82e56e15d980f647765f0da8a9701d",
+    "url": "/my-portfolio-site/index.html"
+  },
+  {
+    "revision": "5b517481ec2c9927a645",
+    "url": "/my-portfolio-site/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b8b36371ee39039c48dd",
+    "url": "/my-portfolio-site/static/css/main.5c1cf2ed.chunk.css"
+  },
+  {
+    "revision": "5b517481ec2c9927a645",
+    "url": "/my-portfolio-site/static/js/2.530b73f6.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/my-portfolio-site/static/js/2.530b73f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8b36371ee39039c48dd",
+    "url": "/my-portfolio-site/static/js/main.fd60651b.chunk.js"
+  },
+  {
+    "revision": "7c394321b2729fd096c3",
+    "url": "/my-portfolio-site/static/js/runtime-main.8944b6c3.js"
+  }
+]);
