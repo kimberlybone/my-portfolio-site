4b60efa4e457b2b549a9cfdfae9276e0e3cfe2d8
kimberlybone <k_bone@uncg.edu>
2020-01-18 17:44:25 -0500

    get request made

--- my-portfolio-backend/app.js
+++ my-portfolio-backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bodyParser = require('bodyParser')
+const bodyParser = require('body-parser')
 // let's us add body rather than parsms so we can add extra info
 const cors = require('cors')
 // cross origin resource sharing. one server or client can request info on your server and cross origin
@@ -9,10 +9,18 @@ const sendGrid = require('@sendGrid/mail')
 const app = express()
 
 // Need to build out server
-app.use(bodyParser.json())
-app.use(cors())
-app.use((req, res, next) => {
+app.use( bodyParser.json() )
+app.use( cors() )
+app.use( (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
 })
+// Defining the routes
+app.get( '/api', ( req, res )=> {
+  // send is a way to send texts
+  res.send('API Status: Running')
+})
+// app.post()
+app.listen(3030, '0.0.0.0')
