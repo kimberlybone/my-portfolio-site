6000270406d6e4d14e0d62c4169d7f9672c86bab
kimberlybone <k_bone@uncg.edu>
2020-01-20 15:24:04 -0500

    changes to bio and cleaning code

--- my-portfolio-site/src/App.js
+++ my-portfolio-site/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Container, Navbar, Nav } from 'react-bootstrap'
 import './App.css';
-// import axios from "axios"
 
 import Footer from './ChildComponents/Footer'
 import WelcomePage from './MainComponents/WelcomePage'
--- my-portfolio-site/src/MainComponents/WhoIAmPage.js
+++ my-portfolio-site/src/MainComponents/WhoIAmPage.js
@@ -8,15 +8,15 @@ function WhoIAmPage( props ){
     <div>
       <Hero title={ props.title } subTitle={ props.subTitle } subText={ props.subText }></Hero>
       <Content>
-        <p> Name: Kimberly Bone </p>
-        <p> Education: University of North Carolina at Greensboro c/o May 2019 | Flatiron School's coding bootcamp c/o November 2019.</p>
-        <p> Hobbies: Cooking, volunteering, photography, and inspiring other women of color </p>
-        <p> Fun Fact: I am bilingual (I speak Spanish).
+        <p> <h4> Name: </h4> Kimberly Bone </p>
+        <p> <h4> Education: </h4> University of North Carolina at Greensboro c/o May 2019 | Flatiron School's coding bootcamp c/o November 2019.</p>
+        <p> <h4> Hobbies: </h4> Cooking, volunteering, photography, and inspiring other women of color </p>
+        <p> <h4> Fun Fact: </h4> - I am bilingual (I speak Spanish).
             I traveled to Costa Rica and loved it so much that I went back a second time.
             The first time to volunteer and the second time to study abroad.
             Both times I stayed with at least 2 different families where they taught us how to live like a Costa Rican. </p>
-        <p> I'm always eager to learn new things. </p>
-        <p> Take a look at my resume! <a href="public/SoftwareResume.pdf" download > Download Kimberly's Resume </a></p>
+          <p> - I'm always eager to learn new things. </p>
+        <p> *Take a look at my resume! <a href="public/SoftwareResume.pdf" download > Download Kimberly's Resume </a></p>
       </Content>
     </div>
   )
