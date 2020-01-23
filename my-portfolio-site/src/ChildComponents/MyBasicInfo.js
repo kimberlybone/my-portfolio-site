6dd7695d50043611bcbb5f14faa1ce58ec00a3a7
kimberlybone <k_bone@uncg.edu>
2020-01-23 18:00:26 -0500

    changed component from content to a container

--- my-portfolio-site/src/ChildComponents/MyBasicInfo.js
+++ my-portfolio-site/src/ChildComponents/MyBasicInfo.js
@@ -8,14 +8,14 @@ function MyBasicInfo( ){
     color: 'white',
     padding: '1.5rem'
   }
-  
+
   return(
     <Spring
       from={{ opacity: 0, marginTop: -500 }}
       to={{ opacity: 1, marginTop: 0 }}
     >
       { props => (
-        <div style={ props }>
+        <div style={ props } >
           <div style={ infoStyle }>
             <h4> Name: </h4>
               <p>  Kimberly Bone </p>
