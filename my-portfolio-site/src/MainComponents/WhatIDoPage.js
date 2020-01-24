d1268e72415e9154e2f99de599b69c4a1704ce3a
kimberlybone <k_bone@uncg.edu>
2020-01-24 16:52:15 -0500

    links work

--- my-portfolio-site/src/MainComponents/WhatIDoPage.js
+++ my-portfolio-site/src/MainComponents/WhatIDoPage.js
@@ -9,10 +9,11 @@ function WhatIDoPage( props ){
       <Hero title={ props.title } subTitle={ props.subTitle } subText={ props.subText }></Hero>
       <Container>
         <h3>Media</h3>
-        <p><a href="https://medium.com/datadriveninvestor/nutrition-meets-artificial-intelligence-b15847d6cc19?source=friends_link&sk=4f62c29ebede52b76b42547149f0ccc4"></a>Nutrition Meets Artificial Intelligence Part 1</p>
-        <p><a href="https://medium.com/@k_bone/nutrition-meets-artificial-intelligence-part-ii-b45f4a000db6?source=friends_link&sk=797b3b6ef097babe8dc31dd4b6897039"/>Nutrition Meets Artificial Intelligence Part 2</p>
-        <p><a href="https://medium.com/swlh/object-orientation-in-javascript-ad25e9e469b7?source=friends_link&sk=ffabe8fdbdc3d86d18dd64945822668b"/>Object-Orientation in JavaScript</p>
-        <p><a href="https://medium.com/swlh/a-glance-at-hooks-aa06027b6b9e?source=friends_link&sk=54260298a91a94b03bd6eff2ae3d4ade"/>React Hooks</p>
+        <p><a href="https://medium.com/datadriveninvestor/nutrition-meets-artificial-intelligence-b15847d6cc19?source=friends_link&sk=4f62c29ebede52b76b42547149f0ccc4">Nutrition Meets Artificial Intelligence Part 1</a></p>
+        <p><a href="https://medium.com/@k_bone/nutrition-meets-artificial-intelligence-part-ii-b45f4a000db6?source=friends_link&sk=797b3b6ef097babe8dc31dd4b6897039">Nutrition Meets Artificial Intelligence Part 2</a></p>
+        <p><a href="https://medium.com/swlh/object-orientation-in-javascript-ad25e9e469b7?source=friends_link&sk=ffabe8fdbdc3d86d18dd64945822668b">Object-Orientation in JavaScript</a></p>
+        <p><a href="https://medium.com/swlh/a-glance-at-hooks-aa06027b6b9e?source=friends_link&sk=54260298a91a94b03bd6eff2ae3d4ade">React Hooks</a></p>
+        <p><a href="https://medium.com/@k_bone/installing-and-understanding-sass-f2cdc15f278a?source=friends_link&sk=5bd02b148f59fc8987928ed117c95deb">Installing and Understanding SASS</a></p>
       </Container>
     </div>
   )
