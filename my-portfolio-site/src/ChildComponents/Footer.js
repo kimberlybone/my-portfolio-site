268484d328300c7c2e2fc13bc81e39983191ec2f
kimberlybone <k_bone@uncg.edu>
2020-01-27 20:06:34 -0500

    added styling to social icons

--- my-portfolio-site/src/ChildComponents/Footer.js
+++ my-portfolio-site/src/ChildComponents/Footer.js
@@ -8,16 +8,22 @@ function Footer(){
     <footer className="mt-5">
       <Container fluid={ true }>
         <Row className="border-top justify-content-between p-3">
-          <Col className="p-0" md={ 3 } sm={ 10 }>
+          <Col className="p-0" md={ 3 } sm={ 3 } style={{ width: '30%' }}>
             Kimberly Bone
           </Col>
-          <Col className='p-1 d-flex justify-content-center' md={ 3 }>
-            <SocialIcon className='social-icon' url='https://github.com/kimberlybone' bgColor='lightgreen'/>
-            <SocialIcon className='social-icon' url='https://www.linkedin.com/in/kimberly-bone-aa3952121/'/>
-            <SocialIcon className='social-icon' url='https://twitter.com/miss_kimB' bgColor='#ff5a01'/>
-            <SocialIcon className='social-icon' url='https://medium.com/@k_bone'/>
+          <Col className='p-1 d-flex justify-content-center' md={ 3 } sm={ 3 } style={{ width: '30%' }}>
+            <SocialIcon url='https://github.com/kimberlybone'
+                        bgColor='lightgreen'
+                        className='social-icon'
+                        style={{ height: 25, width: 25 }}/>
+            <SocialIcon style={{ height: 25, width: 25 }}
+                        url='https://www.linkedin.com/in/kimberly-bone-aa3952121/'/>
+            <SocialIcon style={{ height: 25, width: 25 }}
+                        className='social-icon' url='https://twitter.com/miss_kimB' bgColor='#ff5a01'/>
+            <SocialIcon style={{ height: 25, width: 25 }}
+                        className='social-icon' url='https://medium.com/@k_bone'/>
           </Col>
-          <Col className="p-0 d-flex justify-content-end" md={ 3 } >
+          <Col className="p-0 d-flex justify-content-end" md={ 3 } sm={ 3 } style={{ width: '30%' }}>
             Made with ♡ by Kimberly
           </Col>
         </Row>
