c0f3d67dbdac39cae557dfade03ead7375ed0f8e
kimberlybone <k_bone@uncg.edu>
2020-01-31 11:05:44 -0500

    split fun facts into another paragraph

--- my-portfolio-site/src/ChildComponents/TextForWhat.js
+++ my-portfolio-site/src/ChildComponents/TextForWhat.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Spring } from 'react-spring/renderprops'
 
-
 class TextForWhat extends React.Component {
   render() {
 
@@ -22,8 +21,8 @@ class TextForWhat extends React.Component {
           <div style={ props }>
             <div style={ otherInfoStyle }>
               <h4> Fun Fact: </h4>
-              <p> - I am bilingual (I speak Spanish).
-                    I traveled to Costa Rica and loved it so much that I went back a second time.
+              <p> - I speak Spanish.</p>
+              <p> - I traveled to Costa Rica and loved it so much that I went back a second time.
                     The first time to volunteer and the second time to study abroad.
                     Both times I stayed with at least 2 different families where they taught us how to live like a Costa Rican. </p>
               <p> - I'm always eager to learn new things. </p>
